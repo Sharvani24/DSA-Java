5f1cb31e83f9240b395a1973f2efa0b27b4e9a62
Sharvani Rathore <sharvanirathore2004@gmail.com>
2026-09-17 17:07:13 +0530

    Add files via upload

---
+++ 01-Java-Basics/Day01/PersonalInfo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class  PersonalInfo {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Enter your age: ");
+        int age = sc.nextInt();
+        System.out.println("Enter your height: ");
+        double height = sc.nextDouble();
+        System.out.println("Name: "+ name);
+        System.out.println("Age: "+ age);
+        System.out.println("Height: "+height);
+    }
+}
